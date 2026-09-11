708d3c135ec1ede400d997b83a6fd3d8e745c73d
wpm-git <wpereiramaria@gmail.com>
2026-09-11 06:04:29 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE = 'bjj-note-v2';
+const FILES = ['./', './index.html', './bjj-icon.png', './manifest.webmanifest'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache =>
+      Promise.all(FILES.map(file => cache.add(file).catch(() => null)))
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE)
+            .then(cache => cache.put(event.request, copy))
+            .catch(() => null);
+        }
+        return response;
+      })
+      .catch(async () => {
+        const cached = await caches.match(event.request);
+        if (cached) return cached;
+
+        if (event.request.mode === 'navigate') {
+          return (await caches.match('./index.html')) || (await caches.match('./'));
+        }
+
+        return Response.error();
+      })
+  );
+});
